eaeadd109f0d035c6990e7359088c76e66046e78
Malachi Obadeyi <mobadeyi@googlemail.com>
2021-11-21 15:02:46 +0000

    add event-logger app

---
+++ cmd/web/handlers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    if r.Method != http.MethodPost {
+        w.Header().Set("Allow", http.MethodPost)
+        w.Header().Set("Content-Type", "application/json")
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        app.errorLog.Printf("cant read request body")
+    }
+    app.infoLog.Printf(string(body))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/obady/event-logger
+
+go 1.16
+
+require (
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+)
